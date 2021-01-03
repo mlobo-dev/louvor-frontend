1b2cfa0ddafac83e3299dd2d89176a32a5bcc37c
mlobo-dev <mlobo.dev@gmail.com>
2021-01-03 12:10:43 -0300

    Prototipo da tela de cadastro de musicas

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './card.css';
 
-function Container({ title, content }) {
+function Container({ title, content, menu }) {
   return (
     <>
       <div className="card mx-auto mt-5">
         <div className="card-header text-center">
           <h3>{title}</h3>
         </div>
-        <div className="card-body">{content}</div>
+
+        <div className="card-body">
+          {menu && <div className="mb-2">{menu}</div>}
+          {content}
+        </div>
       </div>
     </>
   );
--- src/components/toastr.js
+++ src/components/toastr.js
@@ -1,34 +1,34 @@
-import toastr from 'toastr'
+import toastr from 'toastr';
 toastr.options = {
-    "closeButton": true,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": true,
-    "positionClass": "toast-top-right",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-}
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: 'toast-top-right',
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: '300',
+  hideDuration: '1000',
+  timeOut: '5000',
+  extendedTimeOut: '1000',
+  showEasing: 'swing',
+  hideEasing: 'linear',
+  showMethod: 'fadeIn',
+  hideMethod: 'fadeOut',
+};
 
 export function mostrarMensagem(titulo, mensagem, tipo) {
-    toastr[tipo](mensagem, titulo)
+  toastr[tipo](mensagem, titulo);
 }
 
 export function mensagemErro(mensagem) {
-    mostrarMensagem('Erro', mensagem, 'error')
+  mostrarMensagem('Erro', mensagem, 'error');
 }
 
 export function mensagemSucesso(mensagem) {
-    mostrarMensagem('Sucesso', mensagem, 'success')
+  mostrarMensagem('Sucesso', mensagem, 'success');
 }
 
 export function mensagemAlerta(mensagem) {
-    mostrarMensagem('Alerta', mensagem, 'warning')
+  mostrarMensagem('Alerta', mensagem, 'warning');
 }
