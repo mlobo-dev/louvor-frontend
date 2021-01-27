19da8278d37422c9b92aff1eca6682c7e4ea93fe
mlobo.dev <mlobo.dev@gmail.com>
2021-01-27 17:56:48 -0300

    controlando estado das listas

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
-import logo from '../../img/logo-sheknah.jpg';
+import logo from '../../img/adsblack.png';
 
 function Navbar() {
   return (
--- src/components/PickList/index.js
+++ src/components/PickList/index.js
@@ -13,58 +13,65 @@ function PickList({
 }) {
   async function adicionarItemSelecionado(id) {
     if (id) {
-      opcoes = opcoes.filter((musica) => musica.id !== id);
+      opcoes = opcoes.filter((musica) => musica.id != id);
       selecionadas.push(await musicaService.buscarMusicaPeloId(id));
       acaoOpcoes(opcoes);
       acaoSelecionadas(selecionadas);
     }
   }
 
-  function renderizarListas() {
-    return (
-      <>
-        <div className="main-container row mx-auto">
-          <div className="list-container col-md-4 col-xs-12 col-sm-12">
-            <h3 className="list-title text-center">{opcoesTitle}</h3>
-            {opcoes &&
-              opcoes.map((musica) => (
-                <>
-                  <div className="list-item">
-                    <li
-                      id={musica.id}
-                      onClick={(e) => adicionarItemSelecionado(e.target.id)}
-                      class="list-group-item list-group-item-action "
-                    >
-                      {musica.nome}
-                    </li>
-                  </div>
-                </>
-              ))}
-          </div>
-
-          <div className="list-container col-md-4 col-xs-12 col-sm-12">
-            <h3 className="list-title text-center">{selecionadasTitle}</h3>
-            {selecionadas &&
-              selecionadas.map((musica) => (
-                <>
-                  <ul>
-                    <div className="list-item">
-                      <li
-                        id={musica.id}
-                        class="list-group-item list-group-item-action "
-                      >
-                        {musica.nome}
-                      </li>
-                    </div>
-                  </ul>
-                </>
-              ))}
-          </div>
-        </div>
-      </>
-    );
+  async function removerItemSelecionado(id) {
+    if (id) {
+      debugger;
+      selecionadas = selecionadas.filter((musica) => musica.id != id);
+      console.log(selecionadas);
+      opcoes.push(await musicaService.buscarMusicaPeloId(id));
+      acaoOpcoes(opcoes);
+      acaoSelecionadas(selecionadas);
+    }
   }
-  return renderizarListas();
+
+  return (
+    <>
+      <div className="main-container row mx-auto">
+        <div className="list-container col-md-4 col-xs-12 col-sm-12">
+          <h3 className="list-title text-center">{opcoesTitle}</h3>
+          {opcoes &&
+            opcoes.map((musica) => (
+              <>
+                <div className="list-item">
+                  <li
+                    id={musica.id}
+                    onClick={(e) => adicionarItemSelecionado(e.target.id)}
+                    class="list-group-item list-group-item-action "
+                  >
+                    {musica.nome}
+                  </li>
+                </div>
+              </>
+            ))}
+        </div>
+
+        <div className="list-container col-md-4 col-xs-12 col-sm-12">
+          <h3 className="list-title text-center">{selecionadasTitle}</h3>
+          {selecionadas &&
+            selecionadas.map((musica) => (
+              <>
+                <div className="list-item">
+                  <li
+                    id={musica.id}
+                    onClick={(e) => removerItemSelecionado(e.target.id)}
+                    class="list-group-item list-group-item-action "
+                  >
+                    {musica.nome}
+                  </li>
+                </div>
+              </>
+            ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PickList;
